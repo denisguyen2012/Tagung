d9777f0e682f9fef652a48543a8e5cba8e343a87
Denis Nguyen <you@example.com>
2015-11-14 14:34:02 +0100

    Main Methode seperat

--- Tagung.java
+++ Tagung.java
@@ -1,21 +1,42 @@
+import java.util.Scanner;
+
 //Leider nicht vervollständigen können..
-public class Tagung {
+public class Tagung<Tagung> {
 	private Tagung tagung;
 	private Tagung [] teilnehmer;
 	private Tagung [] workshops;
 	private Tagung[] rahmenprogramm;
 	private Tagung [] referent;
-	private int wert;
-
-	public void setTagung(Tagung tagung){
-		this.tagung=tagung;
+	private Tagung wert;
+	
+	Scanner scanner = new Scanner(System.in);
+	public Tagung(){
+		
 	}
+	public Tagung(Tagung tagung){
+		this.wert=wert;
+		this.tagung=tagung;
+		
+	}
+
 	
 	public Tagung getTagung(){
 		return tagung;
 	}
+	public void setTagung(Tagung tagung){
+		this.tagung=tagung;
+	}
 
 	public void addTeilnehmer(Tagung[] teilnehmer){
+		System.out.println("Bitte einen Teilnehmernamen eingeben: ");
+		String tn = scanner.next();
+		for(int i =0; i<teilnehmer.length;i++){
+			
+			teilnehmer[i]=(Tagung) tn;
+			System.out.println(i);
+
+		}
+
 	}
 	public void addWorkshops(Tagung[] workshops){
 	}
@@ -24,8 +45,6 @@ public class Tagung {
 	public void addReferent(Tagung[] referent){
 	}
 	
-	public static void main (String [] args){
-		Tagung tg = new Tagung();
 
-	}
+
 }
